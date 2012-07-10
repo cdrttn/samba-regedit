045aed1373113788b19d760831aeb77fbecaf5f6
C. Davis <cd.rattan@gmail.com>
2012-07-10 06:23:33 -0700

    Initial import of treeview code
    Need to make this more Samba-friendly by using talloc where
    neccessary.

--- source3/utils/regedit_treeview.c
+++ source3/utils/regedit_treeview.c
@@ -1,42 +1,39 @@
-/*
- * Samba Unix/Linux SMB client library
- * Registry Editor
- * Copyright (C) Christopher Davis 2012
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ncurses.h>
+#include <menu.h>
 
-#include "regedit_treeview.h"
-#include "lib/registry/registry.h"
+struct tree_node {
 
-struct tree_node *tree_node_new(TALLOC_CTX *ctx, struct tree_node *parent,
-				const char *name, struct registry_key *key)
+	char *name;
+	char *label;
+	
+	struct tree_node *parent;
+	struct tree_node *child_head;
+	struct tree_node *previous;
+	struct tree_node *next;
+};
+
+struct tree_view {
+	
+	struct tree_node *root;
+	WINDOW *window;
+	WINDOW *sub_window;
+	MENU *menu;
+	ITEM **current_items;
+};
+
+struct tree_node *tree_node_new(struct tree_node *parent, const char *name)
 {
 	struct tree_node *node;
 
-	node = talloc_zero(ctx, struct tree_node);
-	if (!node) {
+	node = calloc(1, sizeof(struct tree_node));
+	if (node == NULL)
 		return NULL;
-	}
 
-	node->name = talloc_strdup(node, name);
-	if (!node->name) {
-		talloc_free(node);
-		return NULL;
-	}
-
-	node->key = talloc_steal(node, key);
+	node->name = strdup(name);
 
 	if (parent) {
 		/* Check if this node is the first descendant of parent. */
@@ -69,9 +66,8 @@ struct tree_node *tree_node_pop(struct tree_node **plist)
 		return NULL;
 
 	*plist = node->previous;
-	if (*plist == NULL) {
+	if (*plist == NULL)
 		*plist = node->next;
-	}
 	if (node->previous) {
 		node->previous->next = node->next;
 	}
@@ -99,75 +95,13 @@ struct tree_node *tree_node_first(struct tree_node *list)
 	return list;
 }
 
-bool tree_node_has_children(struct tree_node *node)
+void tree_node_free(struct tree_node *node)
 {
-	const char *classname;
-	uint32_t num_subkeys;
-	uint32_t num_values;
-	NTTIME last_change_time;
-	uint32_t max_subkeynamelen;
-	uint32_t max_valnamelen;
-	uint32_t max_valbufsize;
-	WERROR rv;
-
-	if (node->child_head) {
-		return true;
-	}
-
-	rv = reg_key_get_info(node, node->key, &classname, &num_subkeys,
-			      &num_values, &last_change_time,
-			      &max_subkeynamelen, &max_valnamelen,
-			      &max_valbufsize);
-
-	if (W_ERROR_IS_OK(rv)) {
-		return num_subkeys != 0;
-	}
-
-	return false;
-}
-
-WERROR tree_node_load_children(struct tree_node *node)
-{
-	struct registry_key *key;
-	const char *key_name, *klass;
-	NTTIME modified;
-	uint32_t i;
-	WERROR rv;
-	struct tree_node *new_node, *prev;
-
-	/* does this node already have it's children loaded? */
-	if (node->child_head)
-		return WERR_OK;
-
-	for (prev = NULL, i = 0; ; ++i) {
-		rv = reg_key_get_subkey_by_index(node, node->key, i,
-						 &key_name, &klass,
-						 &modified);
-		if (!W_ERROR_IS_OK(rv)) {
-			if (W_ERROR_EQUAL(rv, WERR_NO_MORE_ITEMS)) {
-				break;
-			}
-
-			return rv;
-		}
-
-		rv = reg_open_key(node, node->key, key_name, &key);
-		if (!W_ERROR_IS_OK(rv)) {
-			return rv;
-		}
-
-		new_node = tree_node_new(node, node, key_name, key);
-		if (new_node == NULL) {
-			return WERR_NOMEM;
-		}
-		
-		if (prev) {
-			tree_node_append(prev, new_node);
-		}
-		prev = new_node;
-	}
-
-	return WERR_OK;
+	assert(node->child_head == NULL);
+	assert(node->next == NULL);
+	assert(node->previous == NULL);
+	free(node->name);
+	free(node);
 }
 
 void tree_node_free_recursive(struct tree_node *list)
@@ -183,80 +117,69 @@ void tree_node_free_recursive(struct tree_node *list)
 			tree_node_free_recursive(node->child_head);
 		}
 		node->child_head = NULL;
-		talloc_free(node);
+		tree_node_free(node);
 	}
 }
 
-static void tree_view_free_current_items(ITEM **items)
+void tree_view_free_current_items(struct tree_view *view)
 {
 	size_t i;
-	struct tree_node *node;
+	struct tree_node *tmp;
 	ITEM *item;
 
-	if (items == NULL) {
+	if (view->current_items == NULL) {
 		return;
 	}
 
-	for (i = 0; items[i] != NULL; ++i) {
-		item = items[i];
-		node = item_userptr(item);
-		if (node && node->label) {
-			talloc_free(node->label);
-			node->label = NULL;
+	for (i = 0; view->current_items[i] != NULL; ++i) {
+		item = view->current_items[i];
+		tmp = item_userptr(item);
+		if (tmp && tmp->label) {
+			free(tmp->label);
+			tmp->label = NULL;
 		}
 		free_item(item);
 	}
-
-	talloc_free(items);
+	free(view->current_items);
+	view->current_items = NULL;
 }
 
-WERROR tree_view_update(struct tree_view *view, struct tree_node *list)
+void tree_view_update(struct tree_view *view, struct tree_node *list)
 {
 	ITEM **items;
-	struct tree_node *node;
+	struct tree_node *tmp;
 	size_t i, n_items;
 
 	if (list == NULL) {
 		list = view->root;
 	}
-	for (n_items = 0, node = list; node != NULL; node = node->next) {
+	for (n_items = 0, tmp = list; tmp != NULL; tmp = tmp->next) {
 		n_items++;
 	}
+	items = calloc(n_items + 1, sizeof(ITEM **));
 
-	items = talloc_zero_array(view, ITEM *, n_items + 1);
-	if (items == NULL) {
-		return WERR_NOMEM;
-	}
-	
-	for (i = 0, node = list; node != NULL; ++i, node = node->next) {
-		const char *label = node->name;
+	for (i = 0, tmp = list; tmp != NULL; ++i, tmp = tmp->next) {
+		const char *label = tmp->name;
 
 		/* Add a '+' marker to indicate that the item has
 		   descendants. */
-		if (tree_node_has_children(node)) {
-			SMB_ASSERT(node->label == NULL);
-			node->label = talloc_asprintf(node, "+%s", node->name);
-			if (node->label == NULL) {
-				goto fail;
-			}
-			label = node->label;
+		if (tmp->child_head) {
+			char buf[32];
+			snprintf(buf, sizeof(buf), "+%s", tmp->name);
+			assert(tmp->label == NULL);
+			tmp->label = strdup(buf);
+			label = tmp->label;
 		}
 
-		items[i] = new_item(label, node->name);
-		set_item_userptr(items[i], node);
+		items[i] = new_item(label, tmp->name);
+		set_item_userptr(items[i], tmp);
 	}
+
 
 	unpost_menu(view->menu);
 	set_menu_items(view->menu, items);
-	tree_view_free_current_items(view->current_items);
+	tree_view_free_current_items(view);
 	view->current_items = items;
-
-	return WERR_OK;
-
-fail:
-	tree_view_free_current_items(items);
-
-	return WERR_NOMEM;
 }
 
 void tree_view_show(struct tree_view *view)
@@ -265,50 +188,21 @@ void tree_view_show(struct tree_view *view)
 	wrefresh(view->window);
 }
 
-static int tree_view_free(struct tree_view *view)
-{
-	if (view->menu) {
-		unpost_menu(view->menu);
-		free_menu(view->menu);
-	}
-	tree_view_free_current_items(view->current_items);
-	tree_node_free_recursive(view->root);
-
-	return 0;
-}
-
-struct tree_view *tree_view_new(TALLOC_CTX *ctx, struct tree_node *root,
-				WINDOW *orig, int nlines, int ncols,
-				int begin_y, int begin_x)
+struct tree_view *tree_view_new(struct tree_node *root, WINDOW *orig,
+	int nlines, int ncols, int begin_y, int begin_x)
 {
 	struct tree_view *view;
-	static const char *dummy = "1234";
+	static const char *dummy = "12345";
 	
-	view = talloc_zero(ctx, struct tree_view);
-	if (view == NULL) {
-		return NULL;
-	}
-
-	talloc_set_destructor(view, tree_view_free);
-
-	view->current_items = talloc_zero_array(ctx, ITEM *, 2);
-	if (view->current_items == NULL) {
-		goto fail;
-	}
-
-	view->current_items[0] = new_item(dummy, dummy);
-	if (view->current_items[0] == NULL) {
-		goto fail;
-	}
-
+	view = calloc(1, sizeof(struct tree_view));
 	view->window = orig;
 	view->sub_window = derwin(orig, nlines, ncols, begin_y, begin_x);
 	view->root = root;
 
+	view->current_items = calloc(2, sizeof(ITEM **));
+	view->current_items[0] = new_item(dummy, dummy);
+
 	view->menu = new_menu(view->current_items);
-	if (view->menu == NULL) {
-		goto fail;
-	}
 	set_menu_format(view->menu, nlines, 1);
 	set_menu_win(view->menu, view->window);
 	set_menu_sub(view->menu, view->sub_window);
@@ -318,14 +212,17 @@ struct tree_view *tree_view_new(TALLOC_CTX *ctx, struct tree_node *root,
 	tree_view_update(view, root);
 
 	return view;
-
-fail:
-	talloc_free(view);
-
-	return NULL;
 }
 
-static void print_path_recursive(WINDOW *label, struct tree_node *node)
+void tree_view_free(struct tree_view *view)
+{
+	unpost_menu(view->menu);
+	free_menu(view->menu);
+	tree_view_free_current_items(view);
+	tree_node_free_recursive(view->root);
+}
+
+void print_path_recursive(WINDOW *label, struct tree_node *node)
 {
 	if (node->parent)
 		print_path_recursive(label, node->parent);
@@ -333,8 +230,7 @@ static void print_path_recursive(WINDOW *label, struct tree_node *node)
 	wprintw(label, "%s/", node->name);
 }
 
-/* print the path of node to label */
-void tree_node_print_path(WINDOW *label, struct tree_node *node)
+void print_path(WINDOW *label, struct tree_node *node)
 {
 	if (node == NULL)
 		return;
